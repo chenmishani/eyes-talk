9029db2dd7c1e7fadf0d88d74475537ffec7f5a4
chen mishani <chen.mishani@gmail.com>
2023-06-08 18:33:33 +0300

    update build for gh pages

--- src/Compo/Humburger.jsx
+++ src/Compo/Humburger.jsx
@@ -7,7 +7,7 @@ export default function Humburger() {
     
         <div className='humburger'>
           <Link to={'/Bracelets'}> <button className='humburgerBtn'>צמידים</button> </Link>
-          <Link to={'/chains'}>  <button className='humburgerBtn' style={{marginRight:'10px'}}>שרשראות</button> </Link>
+          <Link to={'/chains'}>  <button className='humburgerBtn' >שרשראות</button> </Link>
           <Link to={'/contact'} > <button className='humburgerBtn'>צור קשר</button></Link>
           <Link to={'/eyes-talk'}> <button className='humburgerBtn'>בית</button></Link>
           
