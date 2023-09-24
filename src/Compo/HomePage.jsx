b6252e6501048ca7172af3b534971851b0f34a66
chen mishani <chen.mishani@gmail.com>
2023-09-24 15:16:57 +0300

    update build for gh pages

--- src/Compo/HomePage.jsx
+++ src/Compo/HomePage.jsx
@@ -61,6 +61,7 @@ export default function HomePage(props) {
             muted 
             preload="auto"
             loop
+            playsInline
           >
             <source src={homePageVideo} type="video/mp4" />
           </video>
